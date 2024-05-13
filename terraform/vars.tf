9eea0fb9fb6d2ec32aa4251bca90d885d49bf7e0
mehrdad.karami <mehrdadallahkarami@gmail.com>
2024-05-13 15:39:10 +0200

    logs capability to aws iot

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -7,7 +7,7 @@ variable "LAMBDA_FUNCTION_ARN" {
 }
 
 variable "IOT_CERT_ARN" {
-  default = "arn:aws:iot:us-east-1:471112681286:cert/1483ba6ca3a3315d7a50d4e1926ef1490318646b55292af7acba16bdebf134dc"
+  default = "arn:aws:iot:us-east-1:471112681286:cert/76401898d4d0b02b8c2a9a445351b30e0dda7938f6da92c27c908fb57ef624fe"
 }
 
 variable "ACCOUNT_ID" {
